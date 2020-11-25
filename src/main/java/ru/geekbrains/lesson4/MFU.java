83a1523a33c50a0eeab94e551cdcdd9c7612f43e
phil <flip.04@mail.ru>
2020-11-25 16:51:35 +0300

    task2

--- src/main/java/ru/geekbrains/lesson4/MFU.java
+++ src/main/java/ru/geekbrains/lesson4/MFU.java
@@ -1,2 +1,54 @@
-package ru.geekbrains.lesson4;public class MFU {
+package ru.geekbrains.lesson4;
+
+public class MFU {
+    //Создать модель MFU (c возможность сканирования, печати и ксерокопии)
+    private Object scanMonitor = new Object();
+    private Object printMonitor = new Object();
+    private Object sendMonitor = new Object();
+
+    public void copy(){
+        scan();
+        print();
+    }
+
+    private void scaning(){
+        synchronized (scanMonitor) {
+            System.out.println("Process scan started " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(500);
+                System.out.println("Scaning finished " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void scan(){
+        scaning();
+        sendToNet();
+    }
+
+    public void print(){
+        synchronized (printMonitor) {
+            System.out.println("Process print started " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(500);
+                System.out.println("Printing finished " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void sendToNet(){
+        synchronized (sendMonitor) {
+            System.out.println("Start sending to net " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(500);
+                System.out.println("Sending finished " + Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
--- src/main/java/ru/geekbrains/lesson4/MyThread1.java
+++ src/main/java/ru/geekbrains/lesson4/MyThread1.java
@@ -1,2 +1,39 @@
-package ru.geekbrains.lesson4;public class MyThread1 {
+package ru.geekbrains.lesson4;
+
+public class MyThread1 extends Thread {
+    private char currentChar;
+    private char nextChar;
+    private static volatile char staticNextChar = 'A';
+    private static Object monitor = new Object();
+
+    public MyThread1(char currentChar, char nextChar) {
+        this.currentChar = currentChar;
+        this.nextChar = nextChar;
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            printChar();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void printChar() throws InterruptedException {
+
+        for (int i = 0; i < 5; i++) {
+            synchronized (monitor) {
+                while (staticNextChar != currentChar) {
+                    monitor.wait();
+                }
+                System.out.println(currentChar);
+                staticNextChar = nextChar;
+                monitor.notifyAll();
+            }
+
+        }
+    }
 }
